80da1a01ae752e519f286dcac5402aec08fee79f
Mattison Greff <greff.matt@icloud.com>
2018-08-18 15:40:49 -0700

    fix timeout for fake db calls

---
+++ routes/example.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+  router.get("/", (req, res, next) => {
+      setTimeout(() => { //this is to imitate async db queries
+        res.json(db)}, 4000)
+  })
+  
+  return router
+}

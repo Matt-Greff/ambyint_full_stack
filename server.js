f81180e86d6e189a1775d3fe699230cd6748ca8c
Mattison Greff <greff.matt@icloud.com>
2018-08-18 15:41:59 -0700

    add working routes, create fallback(not working yet)

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
+const root       = 'build/public'
 const express    = require('express');
-const root       = `build/public`
 const fallback   = require('express-history-api-fallback');
-const ENV        = process.env.ENV || 'production';
 const bodyParser = require('body-parser');
+const ENV        = process.env.ENV || 'production';
+const db         = require('./routes/fakeDB.json');
 
-const PORT = process.env.API_PORT // this port needs to match the port in the webapack.config.js -> devServer -> proxy
+const PORT = process.env.API_PORT | 8081 // this port needs to match the port in the webapack.config.js -> devServer -> proxy
 
+const exampleRoute   = require('./routes/example.js'  );
 
 const app = express();
+app.use(express.static('build/public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
+app.use('/api/example', exampleRoute(db));
+
+//fallback is always to be listed last
+app.use(fallback('index.html', { root }));
 
 // can be GETted through the webpack-dev-server at localhost:8080/api or whatever host/port makes sense
-app.get('/api', (req, res) => {
-  res.json({pretty: 'much', amazing: 'eh'});
-});
-
-app.listen(PORT, () => {console.log("API server is up");});
-
-
+app.listen(PORT, () => {console.log('server is up :) ' + PORT)});

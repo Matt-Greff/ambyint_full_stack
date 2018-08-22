9294bf4702127601f7fccf53dda3300ebbd921c2
Mattison Greff <greff.matt@icloud.com>
2018-08-22 12:43:13 -0700

    logic for custom search queries

--- server/routes/addresses.js
+++ server/routes/addresses.js
@@ -5,7 +5,8 @@ const router = express.Router();
 module.exports = (db) => {
   router.get('/', async (req, res) => {
     try {
-      const addr = await db.all();
+      const { query } = req.query;
+      const addr = query ? await db.where(query) : await db.all();
       res.json(addr);
     } catch (e) {
       res.json(e);

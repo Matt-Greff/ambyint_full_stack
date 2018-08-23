7ac7428f07becbc9f54d02a16a85ef54799f7497
Mattison Greff <greff.matt@icloud.com>
2018-08-22 21:24:14 -0700

    restructure filtering methods, add location_type filtering, return lat/lng in json

--- server/db/db.js
+++ server/db/db.js
@@ -1,31 +1,70 @@
 const fs = require('fs');
 const parse = require('csv-parse');
+const axios = require('axios');
 
 module.exports = {
-  all(filter) {
+  all() {
     return new Promise((resolve, reject) => {
       const csvData = new Set();
       fs.createReadStream(`${__dirname}/addresses.csv`)
         .pipe(parse())
-        .on('data', (csvrow) => {
-          const escapedRow = csvrow[0].replace(/“(.*)”/g, '$1');
-          // checks filter for match, if no filter is present it rejects empty strings
-          if ((filter && filter(escapedRow)) || (!filter && escapedRow)) csvData.add(escapedRow);
+        .on('data', async (csvrow) => {
+          csvData.add(csvrow[0]);
         })
         .on('error', e => reject(e))
         .on('end', () => resolve([...csvData]));
     });
   },
-  where(query) {
-    return new Promise((res, rej) => {
-      const regExp = new RegExp(`${query}`, 'gi');
-      const filter = str => str.match(regExp);
-      try {
-        const addr = this.all(filter);
-        return res(addr);
-      } catch (e) {
-        return rej(e);
+  queryFilter(addr, query) {
+    const regExp = new RegExp(query, 'gi');
+    return addr.formatted_address.match(regExp);
+  },
+  charFilter(addr) {
+    return addr.replace(/“(.*)”/g, '$1');
+  },
+  async getGeocode(addr) {
+    try {
+      const { data } = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyCykpqmbXtfdpUecLZlA--ftOLQJ-xOLgM`);
+      const { results } = data;
+      const { formatted_address, geometry } = results[0];
+      const { location, location_type } = geometry;
+      const geocodedAddr = {
+        formatted_address,
+        location,
+        location_type,
+      };
+      return geocodedAddr;
+    } catch (e) {
+      return console.log(e);
+    }
+  },
+  validAddrType(addr, type) {
+    return addr.location_type === type;
+  },
+  async addrFilter(arr, { query }) {
+    const processedArr = [];
+    this.charFilter = this.charFilter.bind(this);
+    this.getGeocode = this.getGeocode.bind(this);
+    this.validAddrType = this.validAddrType.bind(this);
+    this.queryFilter = this.queryFilter.bind(this);
+    const {
+      charFilter,
+      getGeocode,
+      validAddrType,
+      queryFilter,
+    } = this;
+    for (let i = 0; i < arr.length; i += 1) {
+      let currAddr = arr[i];
+      currAddr = charFilter(currAddr);
+      currAddr = await getGeocode(currAddr);
+      if (((query && queryFilter(currAddr, query)) || (currAddr && !query)) && validAddrType(currAddr, 'ROOFTOP')) {
+        processedArr.push(currAddr);
       }
-    });
+    }
+    try {
+      return processedArr;
+    } catch (e) {
+      return e;
+    }
   },
 };

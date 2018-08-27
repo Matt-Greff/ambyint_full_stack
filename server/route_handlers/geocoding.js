7167dbf0c2ebfbe2ee32eb39cbfe3b08dc994438
Mattison Greff <greff.matt@icloud.com>
2018-08-26 22:53:25 -0700

    modularize and refactor for testing

--- server/route_handlers/geocoding.js
+++ server/route_handlers/geocoding.js
@@ -4,31 +4,37 @@ module.exports = {
   _getGeocode(addr) { // used then style promises as it dramatically increased effeciency
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyCykpqmbXtfdpUecLZlA--ftOLQJ-xOLgM`)
       .then(({ data }) => {
-        const { results, status } = data;
+        const { status } = data;
         if (status !== 'OK') return { status };
-        const { formatted_address, geometry } = results[0];
-        const { location, location_type } = geometry;
-        const geoAddr = {
-          formatted_address,
-          location,
-          location_type,
-          status,
-        };
-        return geoAddr;
+        return this._formatGeocode(data);
       });
+  },
+  _formatGeocode(data) {
+    const { results, status } = data;
+    const { formatted_address, geometry } = results[0];
+    const { location, location_type } = geometry;
+    return {
+      formatted_address,
+      location,
+      location_type,
+      status,
+    };
   },
   _validLocationType(geo, locationType) {
     return geo.location_type === locationType;
   },
+  _filterLocationType(geocodes, locationType) {
+    return locationType
+      ? geocodes.filter(geo => this._validLocationType(geo, locationType))
+      : geocodes;
+  },
   async _getAndValidateGeo(addresses, options) {
-    this._validLocationType = this._validLocationType.bind(this);
+    this._filterLocationType = this._filterLocationType.bind(this);
     this._getGeocode = this._getGeocode.bind(this);
-    const { locationType } = options;
-    const { _getGeocode, _validLocationType } = this;
+    const { _getGeocode, _filterLocationType } = this;
     const geocodes = addresses.map(addr => _getGeocode(addr));
     const unfilteredGeos = await Promise.all(geocodes);
-    return locationType
-      ? unfilteredGeos.filter(geo => _validLocationType(geo, locationType))
-      : unfilteredGeos;
+    const { locationType } = options;
+    return _filterLocationType(unfilteredGeos, locationType);
   },
 };
